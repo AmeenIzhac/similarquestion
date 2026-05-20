6808f6ac24f8d701dd4da152637581764702d644
Ameen Izhac <ameen.izhacblob@gmail.com>
2026-05-20 10:03:06 +0100

    chore: update PostHog SDK

--- src/posthog.ts
+++ src/posthog.ts
@@ -1,7 +1,8 @@
 import posthog from 'posthog-js'
 
 posthog.init('phc_WOHgnCZE8cPXfL1zyJItWyQOQo7W1VEEXrRrEV7YSHK', {
-  api_host: 'https://app.posthog.com', // or self-hosted URL
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2026-01-30',
   capture_pageview: true,
   capture_pageleave: true,
   session_recording: {
